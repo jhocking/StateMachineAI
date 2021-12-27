59223f893c97027258349055c83c35757ced4da5
Joseph Hocking <jhocking@newarteest.com>
2021-12-27 17:55:07 -0600

    player movement

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,17 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(CharacterController))]
 public class Player : MonoBehaviour
 {
+    public float speed = 10;
+
+    private CharacterController charController;
+
     // Start is called before the first frame update
-    void Start()
-    {
-        
+    void Start() {
+        charController = GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        var xMov = -Input.GetAxis("Vertical") * speed * Time.deltaTime;
+        var zMov = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+        charController.Move(new Vector3(xMov, 0, zMov));
     }
 }
