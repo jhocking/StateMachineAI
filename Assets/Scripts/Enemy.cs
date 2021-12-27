af919428879cbb964f3215f2f981ec67fbabfea9
Joseph Hocking <jhocking@newarteest.com>
2021-12-27 13:45:18 -0600

    navmesh

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,12 +4,14 @@ using System.Collections.Generic;
 using UnityEngine;
 using BasicAI;
 
+[RequireComponent(typeof(UnityEngine.AI.NavMeshAgent))]
 public class Enemy : BaseWaypointAI
 {
     // Start is called before the first frame update
     protected override void Start()
     {
         currentState = new IdleState();
+
         availableStates = new Dictionary<Type, BaseState>() {
             { typeof(IdleState), currentState},
             { typeof(PatrolState), new PatrolState(this.gameObject, waypoints)}
--- Assets/Scripts/PatrolState.cs
+++ Assets/Scripts/PatrolState.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 using BasicAI;
 
 public class PatrolState : BaseState
@@ -11,14 +12,15 @@ public class PatrolState : BaseState
 		PingPong
 	}
 
-	private GameObject actor;
+	private NavMeshAgent agent;
 	private Transform[] waypoints;
 	private PatrolType behavior;
 
 	public PatrolState(GameObject actor, Transform[] waypoints, PatrolType behavior = PatrolType.PingPong) {
-		this.actor = actor;
 		this.waypoints = waypoints;
 		this.behavior = behavior;
+
+		agent = actor.GetComponent<NavMeshAgent>();
 	}
 
     public override Type Tick() {
