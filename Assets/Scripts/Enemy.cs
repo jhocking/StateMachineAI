b00f633a76c01b0810cf78f79538aaafcec7e31b
Joseph Hocking <jhocking@newarteest.com>
2021-12-27 19:31:51 -0600

    dot product field of view

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -12,6 +12,9 @@ public class Enemy : BaseWaypointAI
     public float idleWaitTime = 2;
     public float visionWaitTime = 1;
 
+    [Range(0.1f, 0.9f)]
+    public float facingDotThreshold = .3f; // lower is a wider field of vision
+
     public bool CanSeePlayer { get; private set; }
     public Vector3 SeenPlayerPosition { get; private set; }
 
@@ -37,7 +40,27 @@ public class Enemy : BaseWaypointAI
         yield return new WaitForSeconds(offset);
 
         while (player != null) {
-            // TODO check player line of sight
+            CanSeePlayer = false;
+
+            // first use the dot product to see if the player is within the field of view
+            var playerDir = player.transform.position - this.transform.position;
+            var dot = Vector3.Dot(transform.forward, playerDir);
+            if (dot >= facingDotThreshold) {
+                Debug.Log($"facing {DateTime.UtcNow}");
+
+                // only then do a raycast for line of sight
+                if (Physics.Raycast(transform.position, transform.forward, out var hit)) {
+                    if (hit.transform.gameObject == player) {
+                        CanSeePlayer = true;
+                        SeenPlayerPosition = player.transform.position;
+                    }
+				}
+            }
+
+            if (showRuntimeDebug) {
+                var tint = CanSeePlayer ? Color.green : Color.yellow;
+                Debug.DrawRay(transform.position, transform.forward * 1000, tint);
+            }
 
             // pause before checking again, to simulate reaction time
             yield return new WaitForSeconds(visionWaitTime);
